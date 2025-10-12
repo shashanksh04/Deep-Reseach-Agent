326458702ee1fba07bef9ec42da59c3b30682691
Shashank SH <130638888+shashanksh04@users.noreply.github.com>
2025-10-13 01:34:59 +0530

    Update search_agent.py

--- search_agent.py
+++ search_agent.py
@@ -7,7 +7,7 @@ llama_model = OpenAIChatCompletionsModel(model='mistral:7b', openai_client=ollam
 
 INSTRUCTIONS = (
     "You are a research assistant. Given a search term, you search the web for that term and "
-    "produce a concise summary of the results. The summary must 2-3 paragraphs and less than 300 "
+    "produce a concise summary of the results. The summary must 2-3 paragraphs and less than 150 "
     "words. Capture the main points. Write succintly, no need to have complete sentences or good "
     "grammar. This will be consumed by someone synthesizing a report, so its vital you capture the "
     "essence and ignore any fluff. Do not include any additional commentary other than the summary itself."
